e8608b389af13acd0aad82a6683981e35a12acb0
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-30 11:18:22 +0600

    Create a MongoDB database on mLab and connect with app.

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Just requiring Passport Setup to run it on app load
 require('./config/passport-setup');
 const authRoutes = require('./routes/auth-routes');
+const keys = require('./config/keys');
 
 const app = express();
 
 // Set up a view engine
 app.set('view engine', 'ejs');
+
+// Connect to MongoDB
+mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true }, () => {
+    console.log('connected to MongoDB');
+});
 
 // Set up routes
 app.use('/auth', authRoutes);
