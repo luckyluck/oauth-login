9ac77895e31fbff12c9ab7a62f511afe78538cae
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-30 15:12:30 +0600

    Serialize/deserialize user.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -5,6 +5,18 @@ const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/user-model');
 
+// Putting user to cookie for a browser
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+// Getting user from cookie
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy(
         {
@@ -18,14 +30,14 @@ passport.use(
             User.findOne({ googleId: profile.id }).then(currentUser => {
                 if (currentUser) {
                     // already have the user
-                    console.log('user is:', currentUser);
+                    done(null, currentUser);
                 } else {
                     // if not, create user in our db
                     new User({
                         username: profile.displayName,
                         googleId: profile.id
                     }).save().then(newUser => {
-                        console.log('new user created:', newUser);
+                        done(null, newUser);
                     });
                 }
             });
