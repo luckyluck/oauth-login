ef8c89e43446e136f9918eec2b6d583ee3139a57
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-30 10:30:52 +0600

    Google Strategy setup.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
+const keys = require('./keys');
+
 passport.use(
     new GoogleStrategy({
-        // Options for the Google strategy
+        clientID: keys.clientID,
+        clientSecret: keys.clientSecret
     }),
     () => {
         // Passport callback function
