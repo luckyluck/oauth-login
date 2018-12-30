9063b8be7da8368fc000d396d83b48e1e2c0fabf
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-30 13:52:57 +0600

    Saving User to MongoDB.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20');
 
 // App local key storage
 const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy(
@@ -15,7 +16,13 @@ passport.use(
         (accessToke, refreshToken, profile, done) => {
             // Passport callback function
             console.log('passport callback function fired');
-            console.log(profile)
+            // console.log(profile);
+            new User({
+                username: profile.displayName,
+                googleId: profile.id
+            }).save().then(newUser => {
+                console.log('new user created:', newUser);
+            });
         }
     )
 );
