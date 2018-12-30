69f5ad2c735c53877b7304c2281e74c08865d6a8
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-12-30 10:33:58 +0600

    Securing keys (minor changes).

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,12 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
+// App local key storage
 const keys = require('./keys');
 
 passport.use(
     new GoogleStrategy({
-        clientID: keys.clientID,
-        clientSecret: keys.clientSecret
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
     }),
     () => {
         // Passport callback function
